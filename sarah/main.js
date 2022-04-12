6b7d10c53d77ade22142bd0219d69c2848e47abf
Henry Díaz <61124185+h3nry-d1az@users.noreply.github.com>
2022-04-12 18:56:46 +0200

    Update main.js

--- sarah/main.js
+++ sarah/main.js
@@ -1,4 +1,4 @@
-const {  Client, Intents  } = require('discord.js');
+const {  Client, Intents, Permissions  } = require('discord.js');
 const client = new Client({
   intents:[Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES],
 });
